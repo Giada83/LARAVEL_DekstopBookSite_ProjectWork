6994e84d91eb52914666408be5808de69c8b97e8
Giada Petrillo <157986302+Giada83@users.noreply.github.com>
2024-06-26 19:32:53 +0200

    Factory e Seeder di Review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Book;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,10 @@ class ReviewFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::inRandomOrder()->first()->id,
+            'book_id' => Book::inRandomOrder()->first()->id,
+            'review' => $this->faker->paragraph,
+            'rating' => $this->faker->numberBetween(1, 5),
         ];
     }
 }
