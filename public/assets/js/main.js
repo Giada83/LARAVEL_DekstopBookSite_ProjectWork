8514ff675991683b09ed1f466e2f46eb8ab435ee
Giada Petrillo <157986302+Giada83@users.noreply.github.com>
2024-07-03 13:15:43 +0200

    fixed problema scrolling e body

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,6 @@
 // navbar che cambia colore con altezza calcolata automaticamente
 document.addEventListener("DOMContentLoaded", function () {
     var navbar = document.querySelector(".custom-navbar");
-    var body = document.querySelector("body");
-    body.style.paddingTop = navbar.offsetHeight + "px";
 
     window.addEventListener("scroll", function () {
         if (window.scrollY > 0) {
--- resources/views/template/base.blade.php
+++ resources/views/template/base.blade.php
@@ -1,20 +1,6 @@
-<!doctype html>
-<html lang="en">
+@include('includes.head')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BookMate - @yield('title')</title>
-    {{-- Bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    {{-- Select2 CSS --}}
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet">
-    {{-- Additional styles --}}
-    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
-</head>
-
-<header>@include('includes.navbar')</header>
+<header>@include('partials.navbar')</header>
 
 <body>
     <div class="container mt-4">
