b6ff567c193a21077f8fa36b90c4407b063d6bcb
Giada Petrillo <157986302+Giada83@users.noreply.github.com>
2024-07-10 11:27:42 +0200

    piccole modifiche di stile

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -15,19 +15,17 @@
             <div class="col-auto col-md-2 px-sm-2 px-0 sidebar-container">
                 <div
                     class="d-flex flex-column align-items-center align-items-center align-items-sm-start px-4 pt-2 min-vh-100">
-                    {{-- titolo + img profilo + nome utente + email --}}
-                    <div
-                        class="d-flex align-items-center pb-4 mb-md-3 me-md-auto text-decoration-none flex-column border-bottom border-secondary border-opacity-50 w-100">
+                    {{-- titolo + img profilo + nome utente --}}
+                    <div class="d-flex align-items-center pb-1 mb-md-3 me-md-auto text-decoration-none flex-column">
                         <span
                             class="fs-3 d-none d-sm-inline pt-2 pb-3 fw-medium text-center w-100 purple">ReadWish</span>
-                        <div class="side-img">
-                            <img src="{{ Auth::user()->image_profile }}" alt="image profile">
+                        <div class="side-img mt-1">
+                            <a href="{{ route('dashboard') }}"> <img src="{{ Auth::user()->image_profile }}"
+                                    alt="image profile"></a>
                         </div>
                         <p class="side-p text-wrap w-100 text-center">{{ Auth::user()->name }}</p>
-                        <small
-                            class="card-text text-secondary text-wrap w-100 text-center">{{ Auth::user()->email }}</small>
                     </div>
-                    <ul class="nav nav-pills flex-column mb-0 align-items-center align-items-sm-start sidebar-link "
+                    <ul class="nav nav-pills flex-column mb-0 align-items-center align-items-sm-start sidebar-link mb-sm-auto"
                         id="menu">
                         {{-- home --}}
                         <li class="nav-item w-100">
@@ -64,18 +62,11 @@
                                 <i class="bi bi-person-circle"></i><span
                                     class="ms-1 d-none d-sm-inline">Profile</span></a>
                         </li>
-                    </ul>
-
-                    <hr class="mt-3 mb-3">
-
-                    {{-- logout --}}
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start sidebar-link "
-                        id="menu">
+                        {{-- logout --}}
                         <li>
                             <a class="nav-link px-0 align-middle" href="{{ route('logout') }}"
                                 onclick="event.preventDefault(); document.getElementById('logout-form-navbar').submit();">
-                                <i class="bi bi-box-arrow-right text-secondary"></i><span
-                                    class="ms-1 d-none d-sm-inline text-secondary">Log
+                                <i class="bi bi-box-arrow-right"></i><span class="ms-1 d-none d-sm-inline">Log
                                     Out</span>
                             </a>
                             <form id="logout-form-navbar" action="{{ route('logout') }}" method="POST"
